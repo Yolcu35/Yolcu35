32ecf4ae9a6352ab33b73d0b1ed663cf96e68bca
Yolcu35 <katuran70@gmail.com>
2026-08-14 03:38:39 +0300

    Create SetFilmizle.kt
    package com.aykut.setfilmizle
    
    /**
     * SetFilmizle CloudStream Provider
     *
     * İlk geliştirme aşaması:
     * - Provider yapılandırması
     * - Site adresi
     * - Türkçe dil
     *
     * Sonraki aşamada CloudStream'in güncel extension API'sine
     * gerçek search/load/loadLinks implementasyonu eklenecek.
     */
    object SetFilmizleConfig {
    
        const val BASE_URL = "https://www.setfilmizle.ltd"
        const val PROVIDER_NAME = "SetFilmizle"
        const val LANGUAGE = "tr"
    }

---
+++ SetFilmizle/SetFilmizle/src/main/kotlin/com/aykut/setfilmizle/SetFilmizle.kt
@@ -0,0 +1,19 @@
+package com.aykut.setfilmizle
+
+/**
+ * SetFilmizle CloudStream Provider
+ *
+ * İlk geliştirme aşaması:
+ * - Provider yapılandırması
+ * - Site adresi
+ * - Türkçe dil
+ *
+ * Sonraki aşamada CloudStream'in güncel extension API'sine
+ * gerçek search/load/loadLinks implementasyonu eklenecek.
+ */
+object SetFilmizleConfig {
+
+    const val BASE_URL = "https://www.setfilmizle.ltd"
+    const val PROVIDER_NAME = "SetFilmizle"
+    const val LANGUAGE = "tr"
+}
